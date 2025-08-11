475679047850a143335ac7ac3ab21e567fcbdb15
Benicio Gutierrez <106838587+BenicioGutierrez@users.noreply.github.com>
2025-08-10 23:54:33 -0600

    Create shift_spectra.py

---
+++ training/shift_spectra.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Load original data
+data = np.load('EE_1.npz')
+features = data['features']  # shape: (n_samples, ell)
+ell = data['modes']          # shape: (ell,)
+
+ell = ell.reshape(1, -1)
+
+# Apply the transformation
+transformed = np.log10( (10 ** features) * (2 * np.pi) / (ell * (ell + 1))  )
+
+
+#transformed = (features) * (2 * np.pi) / (ell * (ell + 1))
+
+
+# Save new .npz
+np.savez('EE_1_shifted.npz', features=transformed, modes=ell.flatten())
